7da9234685df5293330e2c7087f1b6e198a5af48
vutienviet <42883535+vutienviet@users.noreply.github.com>
2018-09-01 12:14:21 +0700

    code giao dien

---
+++ Baithu/app/src/main/java/com/example/user/baithu/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.user.baithu;
+
+import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
